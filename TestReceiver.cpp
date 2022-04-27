190757e5d95b544db45c44d9079940e1de8bc7fb
waseem-syed <91089691+waseem-syed@users.noreply.github.com>
2022-04-27 23:10:33 +0530

    remove dups in TestReceiver.cpp

--- TestReceiver.cpp
+++ TestReceiver.cpp
@@ -51,20 +51,25 @@ TEST_CASE("Test Receiver::computeStatics")
    REQUIRE(receiver.m_batteryTemperatureStats.m_averageValue = 99.6);
 }
 
+std::vectot<string> getInputDataForTesting()
+{
+	std::vectot<string> inputDataForTesting{"{ BATTERYAPMS:6, BATTERYTEMPERATURE:149}",
+	                                        "{ BATTERYAPMS:11, BATTERYTEMPERATURE:131}",
+	                                        "{ BATTERYAPMS:3, BATTERYTEMPERATURE:72}",
+	                                        "{ BATTERYAPMS:1, BATTERYTEMPERATURE:100}",
+	                                        "{ BATTERYAPMS:4, BATTERYTEMPERATURE:46}"
+	                                       };
+	return inputDataForTesting;
+}
+
 TEST_CASE("Test Receiver::extractBattertyAmpsReadings")
 {
    Receiver receiver;
-   string dataAsString1 = "{ BATTERYAPMS:6, BATTERYTEMPERATURE:149}";
-   receiver.setReceiverData(dataAsString1);
-   string dataAsString2 = "{ BATTERYAPMS:11, BATTERYTEMPERATURE:131}";
-   receiver.setReceiverData(dataAsString2);
-   string dataAsString3 = "{ BATTERYAPMS:3, BATTERYTEMPERATURE:72}";
-   receiver.setReceiverData(dataAsString3);
-   string dataAsString4 = "{ BATTERYAPMS:1, BATTERYTEMPERATURE:100}";
-   receiver.setReceiverData(dataAsString4);
-   string dataAsString5 = "{ BATTERYAPMS:4, BATTERYTEMPERATURE:46}";
-   receiver.setReceiverData(dataAsString5);
-   
+   std::vectot<string> inputDataList = getInputDataForTesting();
+   for(int index = 0 ; index < inputDataList.size(); index++)
+   {
+	   receiver.setReceiverData(inputDataList[index]);
+   }
    std::vector<int> batteryAmpsList = receiver.extractBattertyAmpsReadings();
    assert(batteryAmpsList.size() == 5);
    REQUIRE(batteryAmpsList[0] == 6);
@@ -77,16 +82,11 @@ TEST_CASE("Test Receiver::extractBattertyAmpsReadings")
 TEST_CASE("Test Receiver::extractBattertyTemperatureReadings")
 {
    Receiver receiver;
-   string dataAsString1 = "{ BATTERYAPMS:6, BATTERYTEMPERATURE:149}";
-   receiver.setReceiverData(dataAsString1);
-   string dataAsString2 = "{ BATTERYAPMS:11, BATTERYTEMPERATURE:131}";
-   receiver.setReceiverData(dataAsString2);
-   string dataAsString3 = "{ BATTERYAPMS:3, BATTERYTEMPERATURE:72}";
-   receiver.setReceiverData(dataAsString3);
-   string dataAsString4 = "{ BATTERYAPMS:1, BATTERYTEMPERATURE:100}";
-   receiver.setReceiverData(dataAsString4);
-   string dataAsString5 = "{ BATTERYAPMS:4, BATTERYTEMPERATURE:46}";
-   receiver.setReceiverData(dataAsString5);
+   std::vectot<string> inputDataList = getInputDataForTesting();
+   for(int index = 0 ; index < inputDataList.size(); index++)
+   {
+	   receiver.setReceiverData(inputDataList[index]);
+   }
 
    std::vector<int> batteryTemperatureList = receiver.extractBattertyTemperatureReadings();
    assert(batteryTemperatureList.size() == 5);
